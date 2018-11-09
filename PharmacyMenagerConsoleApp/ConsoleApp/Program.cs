14548f2766fd3a8bdf04275eac677bcebb01a6b7
Root <inter-net39@o2.pl>
2018-11-09 16:45:37 +0100

    DB-implemented

---
+++ PharmacyMenagerConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //entiti framework
+        }
+    }
+
+    public class Medicine
+    {
+
+    }
+
+    public class Order
+    {
+
+    }
+
+    public class Prescription
+    {
+
+    }
+
+}
